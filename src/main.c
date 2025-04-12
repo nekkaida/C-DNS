1e271d35cacd6b9b4cf46e1f14e94563b0b771d9
Kenneth Riadi Nugroho <kennethriadinugroho@gmail.com>
2025-04-13 04:30:30 +0800

    feat(dns): implement complete DNS message parsing and response

--- src/main.c
+++ src/main.c
@@ -29,6 +29,16 @@ struct dns_header {
     uint16_t arcount; // Number of resource entries
 };
 
+// Structure to hold a parsed question
+struct dns_question {
+    unsigned char *name;      // Pointer to the domain name in buffer
+    size_t name_len;          // Length of the name field including terminator
+    uint16_t type;            // Question type
+    uint16_t class;           // Question class
+    unsigned char *name_uncompressed; // Uncompressed version of name
+    size_t name_uncompressed_len;     // Length of uncompressed name
+};
+
 // Function to convert DNS header to network byte order (big-endian)
 void dns_header_to_network(struct dns_header *header) {
     header->id = htons(header->id);
@@ -45,6 +55,93 @@ void dns_header_from_network(struct dns_header *header) {
     header->ancount = ntohs(header->ancount);
     header->nscount = ntohs(header->nscount);
     header->arcount = ntohs(header->arcount);
+}
+
+// Function to uncompress a compressed domain name
+// Returns a newly allocated buffer with uncompressed name
+unsigned char *uncompress_name(const unsigned char *buffer, const unsigned char *name, size_t *uncompressed_len) {
+    const unsigned char *packet_start = buffer;
+    const unsigned char *current = name;
+    
+    // Estimate maximum size (worst case: completely uncompressed)
+    unsigned char *result = (unsigned char*)malloc(256);
+    unsigned char *result_ptr = result;
+    size_t total_len = 0;
+    
+    while (*current) {
+        if ((*current & 0xC0) == 0xC0) {
+            // This is a pointer (compressed name)
+            uint16_t offset = ((*current & 0x3F) << 8) | *(current + 1);
+            current = packet_start + offset;
+        } else {
+            // This is a regular label
+            uint8_t label_len = *current;
+            
+            // Copy length byte
+            *result_ptr++ = label_len;
+            total_len++;
+            
+            // Copy the label
+            memcpy(result_ptr, current + 1, label_len);
+            result_ptr += label_len;
+            total_len += label_len;
+            
+            // Move to next label
+            current += label_len + 1;
+        }
+    }
+    
+    // Add terminator
+    *result_ptr = 0;
+    total_len++;
+    
+    *uncompressed_len = total_len;
+    return result;
+}
+
+// Function to parse a question from a DNS packet
+// Returns pointer to the byte after the question
+unsigned char *parse_question(const unsigned char *buffer, 
+                              const unsigned char *question_start, 
+                              struct dns_question *question) {
+    unsigned char *current = (unsigned char*)question_start;
+    question->name = current;
+    
+    // Find the end of the domain name
+    while (*current) {
+        if ((*current & 0xC0) == 0xC0) {
+            // This is a compressed name pointer (2 bytes)
+            current += 2;
+            break;
+        } else {
+            // This is a regular label
+            uint8_t label_len = *current;
+            current += label_len + 1;
+        }
+    }
+    
+    // If we didn't end with a pointer, skip the terminating null byte
+    if (*(current - 1) != 0 && (*(current - 2) & 0xC0) != 0xC0) {
+        current++;
+    }
+    
+    // Calculate name field length
+    question->name_len = current - question_start;
+    
+    // Extract QTYPE and QCLASS
+    memcpy(&question->type, current, sizeof(question->type));
+    current += sizeof(question->type);
+    memcpy(&question->class, current, sizeof(question->class));
+    current += sizeof(question->class);
+    
+    // Convert from network byte order
+    question->type = ntohs(question->type);
+    question->class = ntohs(question->class);
+    
+    // Uncompress the name
+    question->name_uncompressed = uncompress_name(buffer, question_start, &question->name_uncompressed_len);
+    
+    return current;
 }
 
 int main() {
@@ -110,8 +207,8 @@ int main() {
         // Convert header from network byte order
         dns_header_from_network(&query_header);
         
-        printf("Received DNS query with ID: %d, OPCODE: %d, RD: %d\n", 
-               query_header.id, query_header.opcode, query_header.rd);
+        printf("Received DNS query with ID: %d, OPCODE: %d, RD: %d, QDCOUNT: %d\n", 
+               query_header.id, query_header.opcode, query_header.rd, query_header.qdcount);
         
         // Create response header with values from the request
         struct dns_header response_header = {0};
@@ -131,8 +228,8 @@ int main() {
             response_header.rcode = 4;         // Not implemented for other opcodes
         }
         
-        response_header.qdcount = 1;           // Include 1 question
-        response_header.ancount = 1;           // Include 1 answer
+        response_header.qdcount = query_header.qdcount; // Same number of questions
+        response_header.ancount = query_header.qdcount; // Same number of answers as questions
         response_header.nscount = 0;
         response_header.arcount = 0;
         
@@ -140,105 +237,109 @@ int main() {
         struct dns_header network_response_header = response_header;
         dns_header_to_network(&network_response_header);
         
-        // Parse the question section from the incoming query
-        unsigned char *question_ptr = buffer + sizeof(struct dns_header);
-        unsigned char *question_start = question_ptr; // Save starting position for copying later
+        // Parse all questions
+        struct dns_question *questions = (struct dns_question*)malloc(query_header.qdcount * sizeof(struct dns_question));
         
-        // Extract the domain name
-        printf("Parsing domain name from question section\n");
-        
-        // Skip through the domain name labels
-        while (*question_ptr != 0) {
-            uint8_t label_length = *question_ptr;
-            question_ptr += label_length + 1; // Move past label and its length byte
+        unsigned char *current_ptr = buffer + sizeof(struct dns_header);
+        for (int i = 0; i < query_header.qdcount; i++) {
+            printf("Parsing question %d\n", i+1);
+            current_ptr = parse_question(buffer, current_ptr, &questions[i]);
+            printf("Question %d: TYPE=%d, CLASS=%d\n", i+1, questions[i].type, questions[i].class);
         }
-        question_ptr++; // Skip the terminating null byte
         
-        // Extract QTYPE and QCLASS (both 2 bytes each)
-        uint16_t qtype, qclass;
-        memcpy(&qtype, question_ptr, sizeof(qtype));
-        question_ptr += sizeof(qtype);
-        memcpy(&qclass, question_ptr, sizeof(qclass));
-        question_ptr += sizeof(qclass);
+        // Calculate response size
+        size_t response_size = sizeof(network_response_header);
         
-        // Convert from network byte order
-        qtype = ntohs(qtype);
-        qclass = ntohs(qclass);
+        // Add space for questions (uncompressed)
+        for (int i = 0; i < query_header.qdcount; i++) {
+            response_size += questions[i].name_uncompressed_len + sizeof(uint16_t) * 2; // name + type + class
+        }
         
-        printf("Parsed question section: TYPE=%d, CLASS=%d\n", qtype, qclass);
+        // Add space for answers
+        for (int i = 0; i < query_header.qdcount; i++) {
+            response_size += questions[i].name_uncompressed_len + // name
+                          sizeof(uint16_t) + // type
+                          sizeof(uint16_t) + // class
+                          sizeof(uint32_t) + // ttl
+                          sizeof(uint16_t) + // data length
+                          sizeof(uint32_t);  // ip address (4 bytes)
+        }
         
-        // Calculate question section length
-        size_t question_len = question_ptr - question_start;
-        
-        // Prepare the answer section using the same domain name from the question
-        // Type = 1 (A record), in network byte order
-        uint16_t answer_type = htons(1);
-        
-        // Class = 1 (IN class), in network byte order
-        uint16_t answer_class = htons(1);
-        
-        // TTL = 60 seconds, in network byte order
-        uint32_t answer_ttl = htonl(60);
-        
-        // Data length = 4 bytes (for IPv4 address), in network byte order
-        uint16_t answer_length = htons(4);
-        
-        // IP address 8.8.8.8 in network byte order
-        uint32_t answer_ip = inet_addr("8.8.8.8");
-        
-        // Calculate the length of the domain name in the question (including terminating zero)
-        size_t domain_name_len = (question_start + question_len) - question_start - sizeof(qtype) - sizeof(qclass);
-        
-        // Calculate total response length
-        size_t answer_section_len = domain_name_len + // Domain name from the question
-                               sizeof(answer_type) + 
-                               sizeof(answer_class) + 
-                               sizeof(answer_ttl) + 
-                               sizeof(answer_length) + 
-                               sizeof(answer_ip);
-        
-        size_t response_len = sizeof(network_response_header) + question_len + answer_section_len;
-        unsigned char response[response_len];
+        // Allocate response buffer
+        unsigned char *response = (unsigned char *)malloc(response_size);
+        if (!response) {
+            printf("Failed to allocate memory for response\n");
+            continue;
+        }
         
         // Copy header to response
         memcpy(response, &network_response_header, sizeof(network_response_header));
+        size_t offset = sizeof(network_response_header);
         
-        // Copy question to response (exactly as received)
-        memcpy(response + sizeof(network_response_header), question_start, question_len);
+        // Copy uncompressed questions to response
+        for (int i = 0; i < query_header.qdcount; i++) {
+            // Copy uncompressed name
+            memcpy(response + offset, questions[i].name_uncompressed, questions[i].name_uncompressed_len);
+            offset += questions[i].name_uncompressed_len;
+            
+            // Copy type (A record = 1)
+            uint16_t type = htons(1);
+            memcpy(response + offset, &type, sizeof(type));
+            offset += sizeof(type);
+            
+            // Copy class (IN = 1)
+            uint16_t class = htons(1);
+            memcpy(response + offset, &class, sizeof(class));
+            offset += sizeof(class);
+        }
         
-        // Copy answer section to response
-        size_t answer_offset = sizeof(network_response_header) + question_len;
-        
-        // Copy the name field (domain name from the question)
-        memcpy(response + answer_offset, question_start, domain_name_len);
-        answer_offset += domain_name_len;
-        
-        // Copy the type field
-        memcpy(response + answer_offset, &answer_type, sizeof(answer_type));
-        answer_offset += sizeof(answer_type);
-        
-        // Copy the class field
-        memcpy(response + answer_offset, &answer_class, sizeof(answer_class));
-        answer_offset += sizeof(answer_class);
-        
-        // Copy the TTL field
-        memcpy(response + answer_offset, &answer_ttl, sizeof(answer_ttl));
-        answer_offset += sizeof(answer_ttl);
-        
-        // Copy the data length field
-        memcpy(response + answer_offset, &answer_length, sizeof(answer_length));
-        answer_offset += sizeof(answer_length);
-        
-        // Copy the IP address field
-        memcpy(response + answer_offset, &answer_ip, sizeof(answer_ip));
+        // Add answers
+        for (int i = 0; i < query_header.qdcount; i++) {
+            // Copy uncompressed name for answer (same as question)
+            memcpy(response + offset, questions[i].name_uncompressed, questions[i].name_uncompressed_len);
+            offset += questions[i].name_uncompressed_len;
+            
+            // Type = 1 (A record), in network byte order
+            uint16_t answer_type = htons(1);
+            memcpy(response + offset, &answer_type, sizeof(answer_type));
+            offset += sizeof(answer_type);
+            
+            // Class = 1 (IN class), in network byte order
+            uint16_t answer_class = htons(1);
+            memcpy(response + offset, &answer_class, sizeof(answer_class));
+            offset += sizeof(answer_class);
+            
+            // TTL = 60 seconds, in network byte order
+            uint32_t answer_ttl = htonl(60);
+            memcpy(response + offset, &answer_ttl, sizeof(answer_ttl));
+            offset += sizeof(answer_ttl);
+            
+            // Data length = 4 bytes (for IPv4 address), in network byte order
+            uint16_t answer_length = htons(4);
+            memcpy(response + offset, &answer_length, sizeof(answer_length));
+            offset += sizeof(answer_length);
+            
+            // IP address (using 8.8.8.8 + question number for uniqueness)
+            uint32_t answer_ip = inet_addr("8.8.8.8") + i;
+            memcpy(response + offset, &answer_ip, sizeof(answer_ip));
+            offset += sizeof(answer_ip);
+        }
         
         // Send response
-        if (sendto(udpSocket, response, response_len, 0, 
+        if (sendto(udpSocket, response, response_size, 0, 
                   (struct sockaddr*)&clientAddress, sizeof(clientAddress)) == -1) {
             perror("Failed to send response");
         } else {
-            printf("Sent DNS response with ID: %d, question section, and answer section\n", response_header.id);
+            printf("Sent DNS response with ID: %d, %d questions and %d answers\n", 
+                   response_header.id, response_header.qdcount, response_header.ancount);
         }
+        
+        // Free memory
+        for (int i = 0; i < query_header.qdcount; i++) {
+            free(questions[i].name_uncompressed);
+        }
+        free(questions);
+        free(response);
     }
     
     close(udpSocket);
